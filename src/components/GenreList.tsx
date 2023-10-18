8e37e528915203a489025e6fd8f787bbc8aacb03
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-10-18 14:08:34 +0200

    display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,25 @@
+import { List, ListItem, Image, Text, HStack } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-url';
 
 function GenreList() {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map(genre => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY={1}>
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              boxSize={8}
+              borderRadius={8}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 }
 export default GenreList;
