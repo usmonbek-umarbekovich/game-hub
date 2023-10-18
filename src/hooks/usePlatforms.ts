a7534c9dc1c70ed95509ee71dfe26ba848a0f511
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-10-18 22:14:28 +0200

    ship platforms with the app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,4 @@
-import useData from './useData';
+import platforms from '../data/platforms';
 
 export interface Platform {
   id: number;
@@ -6,7 +6,9 @@ export interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => {
-  return useData<Platform>('/platforms/lists/parents');
-};
+const usePlatforms = () => ({
+  data: platforms,
+  isLoading: false,
+  error: null,
+});
 export default usePlatforms;
