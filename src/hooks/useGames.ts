81fc6e099d43bd8671a5dde27ea6db2a6c955550
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-10-24 01:42:11 +0200

    implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { GameQuery } from '../App';
 import { Platform } from './usePlatforms';
 import APIClient, { FetchResponse } from '../services/api-client';
@@ -15,17 +15,22 @@ export interface Game {
 const apiClient = new APIClient<Game>('/games');
 
 const useGames = (gameQuery: GameQuery) => {
-  return useQuery<FetchResponse<Game>, Error>({
+  return useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () => {
+    queryFn: ({ pageParam }) => {
       return apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
+          page: pageParam,
         },
       });
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
     },
   });
 };
