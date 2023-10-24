3dd9330beb4e056dc72cbc65446791b51b0fd75e
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-10-24 14:29:32 +0200

    refactor: simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { GameQuery } from '../App';
 import { Platform } from './usePlatforms';
 import APIClient, { FetchResponse } from '../services/api-client';
+import ms from 'ms';
 
 export interface Game {
   id: number;
@@ -32,7 +33,7 @@ const useGames = (gameQuery: GameQuery) => {
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms('24h'),
   });
 };
 
