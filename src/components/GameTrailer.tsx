4eafbd4931ada0e4ac55679e551a577c38f2873e
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-10-25 12:42:02 +0200

    build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,25 @@
+import useTrailers from '../hooks/useTrailers';
+
+interface Props {
+  gameId: number;
+}
+
+function GameTrailer({ gameId }: Props) {
+  const { data: trailers, error, isLoading } = useTrailers(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  const firstTrailer = trailers?.results[0];
+  if (!firstTrailer) return null;
+
+  return firstTrailer ? (
+    <video
+      src={firstTrailer?.data[480]}
+      poster={firstTrailer.preview}
+      controls
+    />
+  ) : null;
+}
+export default GameTrailer;
