77d02eeb91ed0f380cf1fe2a808ba80f58c71334
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-10-25 15:55:28 +0200

    build game screenshots

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,23 @@
+import { Image, SimpleGrid } from '@chakra-ui/react';
+import useScreenshots from '../hooks/useScreenshots';
+
+interface Props {
+  gameId: number;
+}
+
+function GameScreenshots({ gameId }: Props) {
+  const { data: screenshots, isLoading, error } = useScreenshots(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      {screenshots?.results.map(file => (
+        <Image key={file.id} src={file.image}></Image>
+      ))}
+    </SimpleGrid>
+  );
+}
+export default GameScreenshots;
